cd762146b01becfe27a9b7be0162fc01da4b23cd
Mikael Gibert <mikael.gibert@numergy.com>
2015-03-20 19:04:22 +0100

    Fix jshint

--- test/spec/controllers/serverList.js
+++ test/spec/controllers/serverList.js
@@ -30,46 +30,46 @@ describe('Controller: ServerListCtrl', function () {
   it('should retrieve servers on initialization', function () {
 
     var mockData = [{
-        "id": "78ce12f4-c378-4afb-b3ea-d2a2b886652b",
-        "host": "localhost",
-        "sshPort": 22,
-        "user": "mikael",
-        "password": "pass",
-        "privateKey": "",
-        "sudo": true,
-        "sshConnectionValidity": true,
-        "state": "READY"
-    }]
+        'id': '78ce12f4-c378-4afb-b3ea-d2a2b886652b',
+        'host': 'localhost',
+        'sshPort': 22,
+        'user': 'mikael',
+        'password': 'pass',
+        'privateKey': '',
+        'sudo': true,
+        'sshConnectionValidity': true,
+        'state': 'READY'
+    }];
 
     $httpBackend.when('GET', configuration.backend + '/servers').respond(200, mockData);
     $httpBackend.expectGET(configuration.backend + '/servers');
     $httpBackend.flush();
 
     expect(scope.servers.length).toBe(1);
-    expect(scope.servers[0].id).toBe("78ce12f4-c378-4afb-b3ea-d2a2b886652b");
-    expect(scope.servers[0].host).toBe("localhost");
+    expect(scope.servers[0].id).toBe('78ce12f4-c378-4afb-b3ea-d2a2b886652b');
+    expect(scope.servers[0].host).toBe('localhost');
     expect(scope.servers[0].sshPort).toBe(22);
-    expect(scope.servers[0].user).toBe("mikael");
-    expect(scope.servers[0].password).toBe("pass");
-    expect(scope.servers[0].privateKey).toBe("");
+    expect(scope.servers[0].user).toBe('mikael');
+    expect(scope.servers[0].password).toBe('pass');
+    expect(scope.servers[0].privateKey).toBe('');
     expect(scope.servers[0].sudo).toBe(true);
     expect(scope.servers[0].sshConnectionValidity).toBe(true);
-    expect(scope.servers[0].state).toBe("READY");
+    expect(scope.servers[0].state).toBe('READY');
   });
 
   it('should retrieve servers after deletion', function () {
 
     var mockData = [{
-        "id": "78ce12f4-c378-4afb-b3ea-d2a2b886652b",
-        "host": "localhost",
-        "sshPort": 22,
-        "user": "mikael",
-        "password": "pass",
-        "privateKey": "",
-        "sudo": true,
-        "sshConnectionValidity": true,
-        "state": "READY"
-    }]
+        'id': '78ce12f4-c378-4afb-b3ea-d2a2b886652b',
+        'host': 'localhost',
+        'sshPort': 22,
+        'user': 'mikael',
+        'password': 'pass',
+        'privateKey': '',
+        'sudo': true,
+        'sshConnectionValidity': true,
+        'state': 'READY'
+    }];
 
     $httpBackend.when('GET', configuration.backend + '/servers').respond(200, mockData);
     $httpBackend.expectGET(configuration.backend + '/servers');
