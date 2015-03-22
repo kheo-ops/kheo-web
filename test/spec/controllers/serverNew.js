ce1a7721674575b85bc3210809a67a9780ad74c6
Mikael Gibert <mikael.gibert@gmail.com>
2015-03-22 18:25:54 +0100

    Adding tests to serverNewController

--- test/spec/controllers/serverNew.js
+++ test/spec/controllers/serverNew.js
@@ -1,21 +1,69 @@
 'use strict';
 
 describe('Controller: ServerNewCtrl', function () {
+  var $httpBackend, $rootScope, $resource, configuration, $controller, $location, _, ServerNewCtrl;
+  var scope;
 
   // load the controller's module
   beforeEach(module('kheoApp'));
 
-  var ServernewCtrl,
-    scope;
+  beforeEach(inject(function($injector) {
+    $httpBackend = $injector.get('$httpBackend');
+    $rootScope = $injector.get('$rootScope');
+    $resource = $injector.get('$resource');    
+    $controller = $injector.get('$controller');
+    $location = $injector.get('$location');
+    configuration = $injector.get('configuration');
+    _ = $injector.get('_');
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    ServernewCtrl = $controller('ServerNewCtrl', {
-      $scope: scope
-    });
+    scope = $rootScope.$new();    
+    ServerNewCtrl = $controller('ServerNewCtrl', {
+      '$scope': scope,
+      '$resource': $resource,
+      '$location': $location,
+      'configuration': configuration,      
+      '_': _
+    });    
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {    
+  afterEach(function() {
+     $httpBackend.verifyNoOutstandingExpectation();
+     $httpBackend.verifyNoOutstandingRequest();
+   });
+
+  it('should retrieve plugins on initialization', function () {
+    $httpBackend.when('GET', configuration.backend + '/plugins').respond(200, []);
+    $httpBackend.expectGET(configuration.backend + '/plugins');
+    $httpBackend.flush();
+  });
+
+  it('should not save server if it is undefined', function () {
+    $httpBackend.when('GET', configuration.backend + '/plugins').respond(200, []);
+    $httpBackend.expectGET(configuration.backend + '/plugins');
+    
+    scope.save();
+    $httpBackend.flush();
+  });
+
+  it('should save server if it is defined', function () {    
+    $httpBackend.when('GET', configuration.backend + '/plugins').respond(200, []);
+    $httpBackend.expectGET(configuration.backend + '/plugins');
+    
+    $httpBackend.when('POST', configuration.backend + '/servers').respond(201);
+    $httpBackend.expectPOST(configuration.backend + '/servers');
+    
+    scope.server = {
+      'id': '78ce12f4-c378-4afb-b3ea-d2a2b886652b',
+      'host': 'localhost',
+      'sshPort': 22,
+      'user': 'mikael',
+      'password': 'pass',
+      'privateKey': '',
+      'sudo': true,
+      'sshConnectionValidity': true,
+      'state': 'READY'
+    };
+    scope.save();
+    $httpBackend.flush();
   });
 });
